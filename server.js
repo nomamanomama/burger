dd6e9b70137956fc4e785d257283ad2a8e639d76
nomamanomama <33500937+nomamanomama@users.noreply.github.com>
2018-05-01 11:44:28 -0500

    working version without create new burger

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+//load environment variables from .env file
+require('dotenv').config();
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+//serve static files from public directory
+app.use(express.static("public"));
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on PORT: " + PORT);
+});
